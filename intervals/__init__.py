fb6ea0e680db2405a1cbf233404599e963e4d892
Marcnuth <hxianxian@qq.com>
2018-01-03 19:46:22 +0800

    rename the module name

---
+++ intervals/__init__.py
@@ -0,0 +1,9 @@
+from .basic_interval import Interval
+from .float_interval import FloatInterval
+from .datetime_interval import DatetimeInterval
+
+__all__ = [
+    'Interval',
+    'FloatInterval',
+    'DatetimeInterval'
+]
