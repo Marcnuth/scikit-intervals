c169be471dac2b070b37abc72264a652fb7a3d4b
Marcnuth <hxianxian@qq.com>
2018-01-03 19:54:52 +0800

    refactor directory structure

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scikit-intervals',
-    version='0.1.1',
+    version='0.1.2',
 
     description=("handling intervals calculation in python"),
 
