0ec250e5ba05b8d8bb340d9e142ce4cf4d2e8a78
Marcnuth <hxianxian@qq.com>
2018-01-03 17:15:53 +0800

    add setup files

---
+++ setup.py
@@ -0,0 +1,42 @@
+'''
+Setup Scikit-intervals
+'''
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='scikit-intervals',
+    version='0.1.0',
+
+    description=("handling intervals calculation in python"),
+
+    url="https://github.com/Marcnuth/scikit-intervals",
+
+    # Author details
+    author="Marcnuth",
+    author_email="marcnuth@foxmail.com",
+
+    license="Apache License 2.0",
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 3'
+
+    ],
+
+    # What does your project relate to?
+    keywords=("intervals", "scikit", "mathematics"),
+
+    packages=find_packages("."),
+
+    install_requires=[
+        "numpy",
+        "arrow"
+    ],
+
+)
